c87a6ddc615a9402c2ab9ae4bda975b139903a17
smartemmy100 <emmanuelbayo1000@gmail.com>
2023-08-11 14:17:42 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry level stage
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	char m;
+
+	m = 'a';
+	while
+		(m < = 'z') (
+				putchar(m);
+				c++;
+			}
+		putchar("\n");
+return (0);
+}
