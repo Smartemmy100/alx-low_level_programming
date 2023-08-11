0dee471225ae813400e467dcdce07dd23855e947
smartemmy100 <emmanuelbayo1000@gmail.com>
2023-08-11 16:08:39 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry level stage
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	char m;
+	char n;
+
+	m = 'b';
+	n = 'B';
+	while
+		(m <= 'z')   {
+			putchar(m);
+			m++;
+		}
+	while
+		(n <= 'z')   {
+			putchar(n);
+			n++;
+		}
+	putchar("\n");
+return (0);
+}
