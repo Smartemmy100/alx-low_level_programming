f97cd17e8280f0174d00411d029e95641faa4bd6
smartemmy100 <emmanuelbayo1000@gmail.com>
2023-08-11 18:32:51 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry level stage
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <10; digit++)
+		printf("%d", digit);
+	printf("\n");
+	return (0);
+}
