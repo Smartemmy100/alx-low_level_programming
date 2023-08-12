05f3c5389956abd9779fa24e6406fe1840a6573a
smartemmy100 <emmanuelbayo1000@gmail.com>
2023-08-11 17:26:52 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry level stage
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	char e;
+
+	e = a;
+	while
+		(e <= 'z') {
+			putchar(e);
+			e++;
+		}
+	putchar('\n');
+return (0);
+}
