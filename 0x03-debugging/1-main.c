6ffa447839c3983272dd2d2fc00accc21c013505
smartemmy100 <emmanuelbayo1000@gmail.com>
2023-08-17 11:13:13 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,8 +11,8 @@ int main(void)
 	i = 0;
 	/*while (i < 10)*/
 	/*{*/
-		/*putchar(i);*/
-		/*{*/
-			printf("infinite loop avoided! \\o/\n");
+	/*putchar(i);*/
+	/*{*/
+	printf("infinite loop avoided! \\o/\n");
 	return (0);
 }
