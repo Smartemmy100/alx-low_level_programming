11184ecc4632589577984a9cd29b10274da48b03
smartemmy100 <emmanuelbayo1000@gmail.com>
2023-08-16 18:05:53 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,31 +5,32 @@
 void times_table(void)
 {
 	int i, j, k;
-	
-	for (i = 0; i < 10; i++)
+
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (j = 0; j <= 9; j++)
 		{
-			k = j * i;
-			if (j == 0)
+			k = (j * i);
+			if ((k / 10) > 0)
 			{
-				_putchar(k + '0');
-			}
-			if (k < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((k % 10) + '0');
+			if (j < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
-		_putchar('\n'):
 	}
+		_putchar('\n');
 }
 
