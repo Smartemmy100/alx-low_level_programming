4f5cdb6eeaee99dca70695198525248900e6650f
smartemmy100 <emmanuelbayo1000@gmail.com>
2023-08-20 23:28:49 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: 0 (successful)
+      */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Asaac Asimov";
+	print_rev(str);
+return (0);
+}
