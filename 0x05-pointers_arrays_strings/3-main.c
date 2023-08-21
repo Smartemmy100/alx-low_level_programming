a69cd3818d09076aac190f0dec708518c706f505
smartemmy100 <emmanuelbayo1000@gmail.com>
2023-08-20 23:13:49 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Emmanuel Bayo";
+	_puts(str);
+return (0);
+}
