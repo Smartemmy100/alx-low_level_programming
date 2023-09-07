91a1b2bb4f7d5808ea76d8072bb97ad9bf18012f
smartemmy100 <emmanuelbayo1000@gmail.com>
2023-09-07 16:14:57 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,40 +12,49 @@
  * @old_size: the size in bytes of the allocated space for ptr
  * @new_size: the new size in bytes of the new memory block
  * Return: a pointer to the new memory block
-        */
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *temp_block;
 	unsigned int i;
 
-	/* If new_size is equal to zero, and ptr is not NULL, then the call is equivalent to free(ptr) */
-	if (new_size == 0 && ptr != NULL) {
+ /* If new_size is equal to zero, and ptr is not NULL,) */
+	if (new_size == 0 && ptr != NULL)
+	{
 		free(ptr);
-		return NULL;
+		return (NULL);
 	}
 	/* If ptr is NULL, then the call is equivalent to malloc(new_size) */
-	else if (ptr == NULL) {
-		return malloc(new_size);
+	else if (ptr == NULL)
+	{
+		return (malloc new_size);
 	}
-	/* If new_size == old_size do not do anything and return ptr */
-	else if (new_size == old_size) {
-		return ptr;
+ /* If new_size == old_size do not do anything and return ptr */
+	else if (new_size == old_size)
+	{
+		return (ptr);
 	}
-	/* Allocate a new block of memory with the specified new size */
-	else {
+ /* Allocate a new block of memory with the specified new size */
+	else
+	{
 		temp_block = malloc(new_size);
-		if (temp_block != NULL) {
-			/* Copy the memory contents from the old block to the new block */
-								            /* Use the smaller of old_size and new_size to avoid over-copying */
+		if (temp_block != NULL)
+		{
+ /* Copy the memory contents from the old block to the new block */
+/* Use the smaller of old_size and new_size to avoid over-copying */
 			unsigned int copy_size = MIN(old_size, new_size);
-			for (i = 0; i < copy_size; i++) {
+
+			for (i = 0; i < copy_size; i++)
+			{
 				*((char *)temp_block + i) = *((char *)ptr + i);
 			}
-			/* Free the old memory block */
+ /* Free the old memory block */
 			free(ptr);
-			return temp_block;
-		} else {
+			return (temp_block);
+		}
+		else
+		{
 		}
 	}
-return NULL;
+return (NULL);
 }
