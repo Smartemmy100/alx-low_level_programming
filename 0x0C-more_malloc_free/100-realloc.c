0961855e89ed06b557f137898fcb224fbd9373ae
smartemmy100 <emmanuelbayo1000@gmail.com>
2023-09-08 04:04:37 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *temp_block;
 	unsigned int i;
 
-	/* If new_size is equal to zero, and ptr is not NULL, then the call is equivalent to free(ptr) */
+	/* If new_size is equal to zero, and ptr is not NULL,) */
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -27,12 +27,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	/* If ptr is NULL, then the call is equivalent to malloc(new_size) */
 	else if (ptr == NULL)
 	{
-		return malloc(new_size);
+		return malloc (new_size);
 	}
 	/* If new_size == old_size do not do anything and return ptr */
 	else if (new_size == old_size)
 	{
-		return ptr;
+		return (ptr);
 	}
 	/* Allocate a new block of memory with the specified new size */
 	else
@@ -43,17 +43,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			/* Copy the memory contents from the old block to the new block */
 			/* Use the smaller of old_size and new_size to avoid over-copying */
 			unsigned int copy_size = MIN(old_size, new_size);
+
 			for (i = 0; i < copy_size; i++)
 			{
 				*((char *)temp_block + i) = *((char *)ptr + i);
 			}
 			/* Free the old memory block */
 			free(ptr);
-			return temp_block;
+			return (temp_block);
 		}
 		else
 		{
-			return NULL;
+			return (NULL);
 		}
 	}
 }
